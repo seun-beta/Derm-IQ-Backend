9144c4970ba30ef3f26c7f876ff92272651d8faa
Seunfunmi Adegoke <adegokeseunfunmi1999@gmail.com>
2024-02-14 14:53:55 +0100

    Write schema for user auth logic
    Signed-off-by: Seunfunmi Adegoke <adegokeseunfunmi1999@gmail.com>

---
+++ app/users/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+
+class RegisterUser(BaseModel):
+    first_name: str
+    last_name: str
+    email_address: EmailStr
+    mobile_number: str
+    password: str
+
+
+class LoginUser(BaseModel):
+    email_address: EmailStr | None
+    mobile_number: str
+    password: str
