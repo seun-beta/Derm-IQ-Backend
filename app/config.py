b0fe89aa156200a25b70c9ec96838ea6b1cb3312
Seunfunmi Adegoke <adegokeseunfunmi1999@gmail.com>
2024-08-11 09:10:07 +0100

    Implement db config
    Signed-off-by: Seunfunmi Adegoke <adegokeseunfunmi1999@gmail.com>

--- app/config.py
+++ app/config.py
@@ -2,10 +2,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str
+    openai_api_key: str
     jwt_algorithm: str
     jwt_secret: str
     jwt_access_token_exp_minutes: int
+    database_url: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- app/database.py
+++ app/database.py
@@ -1,20 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from mongoengine import Document, connect
 
 from app.config import settings
 
-engine = create_engine(settings.database_url)
+# Connect to MongoDB using MongoEngine
+connect(db=settings.database_name, host=settings.database_url)
 
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class Base(Document):
+    meta = {"abstract": True}
 
-Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
